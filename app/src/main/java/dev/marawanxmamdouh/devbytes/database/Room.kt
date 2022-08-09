1827f995f6c41fd45ccb6ecc902655b6c0a74ff9
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-09 19:57:17 +0200

    Create the Doa

--- app/src/main/java/dev/marawanxmamdouh/devbytes/database/Room.kt
+++ app/src/main/java/dev/marawanxmamdouh/devbytes/database/Room.kt
@@ -1,1 +1,16 @@
 package dev.marawanxmamdouh.devbytes.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
