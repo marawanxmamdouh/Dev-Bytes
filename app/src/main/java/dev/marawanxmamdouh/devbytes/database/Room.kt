9c1768bb211cb57b453e8d817a5f8a302232dc4c
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-09 19:57:49 +0200

    Create the Database

--- app/src/main/java/dev/marawanxmamdouh/devbytes/database/Room.kt
+++ app/src/main/java/dev/marawanxmamdouh/devbytes/database/Room.kt
@@ -1,10 +1,10 @@
 package dev.marawanxmamdouh.devbytes.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
 
 @Dao
 interface VideoDao {
@@ -13,4 +13,26 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+@OptIn(InternalCoroutinesApi::class)
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
 }
