66c491580e71206901b9dcc2880f4e0107187e81
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-11 12:40:49 +0200

    Create a Worker
    pre-fetch data when the app is in the background

--- app/src/main/java/dev/marawanxmamdouh/devbytes/work/RefreshDataWork.kt
+++ app/src/main/java/dev/marawanxmamdouh/devbytes/work/RefreshDataWork.kt
@@ -1,1 +1,28 @@
 package dev.marawanxmamdouh.devbytes.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dev.marawanxmamdouh.devbytes.database.getDatabase
+import dev.marawanxmamdouh.devbytes.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
